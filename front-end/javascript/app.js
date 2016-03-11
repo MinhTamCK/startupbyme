b5863a9d454c342db988d3d777cefc300a9cad83
tam.nm <tam.nm@sutrixmedia.com>
2016-03-11 15:13:29 +0700

    Add contact block.

--- front-end/javascript/app.js
+++ front-end/javascript/app.js
@@ -1,15 +1,15 @@
 /* particlesJS.load(@dom-id, @path-json*/
 var flag = true;
 particlesJS.load('stu-particles', '../javascript/data/particles.json');
-setInterval(function() {
-    if (flag) {
-        particlesJS.load('stu-particles', '../javascript/data/particles-2.json');
-        flag = false;
-    } else {
-        particlesJS.load('stu-particles', '../javascript/data/particles.json');
-        flag = true;
-    }
-}, 10000);
+// setInterval(function() {
+//     if (flag) {
+//         particlesJS.load('stu-particles', '../javascript/data/particles-2.json');
+//         flag = false;
+//     } else {
+//         particlesJS.load('stu-particles', '../javascript/data/particles.json');
+//         flag = true;
+//     }
+// }, 10000);
 
 // Trianglify create
 var pattern = Trianglify({
@@ -17,3 +17,11 @@ var pattern = Trianglify({
     height: 500
 });
 $('.stu-trianglify').append(pattern.canvas());
+// Contact
+var trianglifyContact = Trianglify({
+    width: window.innerWidth,
+    variance: "0",
+    seed: '4txiq',
+    x_colors: 'random'
+});
+$('.stu-trianglify-contact').append(trianglifyContact.canvas());
